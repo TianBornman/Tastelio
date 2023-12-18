460cef299abfabe3cf37b1fd13a257508a685ab9
Tian Bornman <tianbornman.tb@gmail.com>
2023-12-18 21:52:01 +0200

    Setting up workflow

---
+++ Tastelio/Tastelio/ViewModel/Home/HomeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using DAL.Services.SqlService;
+using Dapper;
+using Models.Models;
+
+namespace Tastelio.ViewModel.Home
+{
+	public class HomeViewModel : BaseViewModel, IHomeViewModel
+	{
+		public HomeViewModel(ISqlService database) : base(database)
+		{
+		}
+
+		public async Task<User?> GetRecord(string? search)
+		{
+			string sql = "SELECT * FROM Users WHERE Firstname = @Search OR @Search IS NULL";
+
+			DynamicParameters dynamicParams = new();
+			dynamicParams.Add("@Search", search);
+
+			return await database.GetSingle<User>(sql, dynamicParams);
+		}
+	}
+}
---
+++ Tastelio/Tastelio/ViewModel/Home/IHomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Models.Models;
+
+namespace Tastelio.ViewModel.Home
+{
+	public interface IHomeViewModel
+	{
+		Task<User?> GetRecord(string? search);
+	}
+}
