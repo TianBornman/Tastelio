e777a06a028f4e52a61a29524ebcc7f820000704
Tian Bornman <tianbornman.tb@gmail.com>
2024-05-12 20:25:55 +0200

    Adding AutoMapper

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,6 +1,7 @@
 using Persistance;
 using Infrastructure;
 using Application;
+using Api.AutoMapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure();
 builder.Services.AddPersistance(builder.Configuration);
+
+builder.Services.AddAutoMapper();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- Application/Handlers/User/Create/CreateUserCommandHandler.cs
+++ Application/Handlers/User/Create/CreateUserCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Application.DataTransferObjects;
+using AutoMapper;
 using Domain.Repositories;
 using MediatR;
 
@@ -7,34 +8,23 @@ namespace Application.Handlers.User.Create;
 public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, UserDto>
 {
     private readonly IUnitOfWork uow;
+    private readonly IMapper mapper;
 
-    public CreateUserCommandHandler(IUnitOfWork uow)
+    public CreateUserCommandHandler(IUnitOfWork uow, IMapper mapper)
     {
         this.uow = uow;
+        this.mapper = mapper;
     }
 
     public async Task<UserDto> Handle(CreateUserCommand request, CancellationToken cancellationToken)
     {
-        var entity = new Domain.Entities.User()
-        {
-            Email = request.Email,
-			FirstName = request.FirstName,
-	        LastName = request.LastName,
-	        Password = request.Password,
-			PasswordSalt = request.PasswordSalt
-        };
+        var entity = mapper.Map<Domain.Entities.User>(request);
 
         await uow.UserRepository.Add(entity);
 
         await uow.SaveChanges();
 
-		var user = new UserDto()
-		{
-            Id = entity.Id,
-			Email = entity.Email,
-			FirstName = entity.FirstName,
-			LastName = entity.LastName,
-		};
+		var user = mapper.Map<UserDto>(entity);
 
 		return user;
     }
